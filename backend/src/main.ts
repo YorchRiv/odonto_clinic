5f40dfb2d17001fcd2ed2b04307bf9037362aa1f
cáceres476 <alakycm@hotmail.com>
2025-09-11 18:55:10 -0600

    Se actualizó .gitignore y se modificó main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000); // Use the PORT environment variable or default to 3000
 }
 bootstrap();
