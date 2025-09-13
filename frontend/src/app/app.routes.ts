3bfb01d4c381771f6147415006a53f196d4a57ec
Jorge Mejicanos <jorgemejicanos145@gmail.com>
2025-09-12 19:20:45 -0600

    component dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+export const routes: Routes = [
+	{
+		path: 'dashboard',
+		component: DashboardComponent
+	}
+];
