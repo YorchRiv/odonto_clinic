bafadc3134b98b23d744b84da5289641e5c32664
cáceres476 <alakycm@hotmail.com>
2025-09-13 02:05:28 -0600

    creacion de componentes principales, layout principal con sidebar y router-outlet
    - Se generaron los componentes: Pacientes, Historia Clínica, Reportes y Configuración.
    - Se configuraron las rutas en app.routes.ts para cada nuevo componente.
    - Se implementó un sidebar con navegación usando routerLink y routerLinkActive.
    - Se añadieron estilos globales para resaltar la opción activa y mejorar el diseño del sidebar.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,10 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AgendaComponent } 	from './agenda/agenda.component'; // Importa el componente de la agenda primer letra mayuscula y la C
+import { PacientesComponent } from './pacientes/pacientes.component'; // Importa el componente de pacientes
+import { HistoriaClinicaComponent } from './historia-clinica/historia-clinica.component';
+import { ReportesComponent } from './reportes/reportes.component'; // Importa el componente de historia clinica
+import { ConfiguracionComponent } from './configuracion/configuracion.component'; // Importa el componente de configuracion
 
 export const routes: Routes = [
 	{
@@ -16,5 +20,21 @@ export const routes: Routes = [
 	{
 		path: 'agenda',
 		component: AgendaComponent
+	},
+	{
+		path: 'pacientes',
+		component: PacientesComponent
+	},
+	{
+		path: 'historia-clinica',
+		component: HistoriaClinicaComponent	
+	},
+	{
+		path: 'reportes',
+		component: ReportesComponent
+	},
+	{
+		path: 'configuracion',
+		component: ConfiguracionComponent
 	}
 ];
