094f49dc4f77c1662f47c0a99c0cbf8340aba1cf
cáceres476 <alakycm@hotmail.com>
2025-09-12 23:08:59 -0600

    component agenda
    se crea el componente agenda

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,18 @@
 
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AgendaComponent } 	from './agenda/agenda.component'; // Importa el componente de la agenda primer letra mayuscula y la C
 
 export const routes: Routes = [
 	{
 		path: 'dashboard',
 		component: DashboardComponent
+	},
+	{
+		path: 'agenda',
+		component: AgendaComponent
 	}
+
+
 ];
+
